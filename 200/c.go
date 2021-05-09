f7f088b61f4b2c213f54f6280e0fd6f31ed7c8e7
konatsup <konatsukomatsu@gmail.com>
2021-05-09 16:21:56 +0900

    fix 200c

--- 200/c.go
+++ 200/c.go
@@ -6,17 +6,15 @@ func main() {
 	var n int
 	fmt.Scan(&n)
 	a := make([]int, n)
+	b := make([]int, 200)
 	for i := 0; i < n; i++ {
 		fmt.Scan(&a[i])
+		b[a[i]%200]++
 	}
 
 	cnt := 0
-	for i := 0; i < n; i++ {
-		for j := i + 1; j < n; j++ {
-			if (a[i]-a[j])%200 == 0 {
-				cnt++
-			}
-		}
+	for i := 0; i < 200; i++ {
+		cnt += (b[i]*(b[i]-1))/2
 	}
 	fmt.Println(cnt)
 }
